a8ba8e67d2d222fa8dde4680771c10b2889fa4dd
David Andersen <git@davidandersen.us>
2025-08-07 11:24:10 -0700

    add system if it doesnt exist

--- scanner/command/update_market.py
+++ scanner/command/update_market.py
@@ -1,24 +1,35 @@
 from dataclasses import dataclass
 import logging
-from scanner.repo.market_repository import MarketRepository
+
+from scanner.entity.system import Point3D, System
+from scanner.repo.market_repository import ResourceNotFoundError
 from scanner.repo.system_repository import SystemRepository
 
 
 @dataclass
-class UpdateMarketRequest:
-    market_id: int
+class UpdateSystemRequest:
+    address: int
+    name: str
+    position: Point3D
 
 
-class UpdateMarket:
+class UpdateSystem:
     log = logging.getLogger(__name__)
 
     def __init__(
         self,
-        market_repository: MarketRepository,
         system_repository: SystemRepository,
     ):
         self.system_repository = system_repository
-        self.market_repository = market_repository
 
-    def execute(self, request: UpdateMarketRequest):
-        pass
+    def execute(self, request: UpdateSystemRequest):
+        try:
+            self.system_repository.get_system_by_name(request.name)
+        except ResourceNotFoundError:
+            self.system_repository.create(
+                System(
+                    address=request.address,
+                    name=request.name,
+                    position=request.position,
+                )
+            )
---
+++ tests/command/test_update_system.py
@@ -0,0 +1,68 @@
+from hamcrest import assert_that, equal_to
+import pytest
+
+from scanner.command.update_market import UpdateSystem, UpdateSystemRequest
+from scanner.entity.system import Point3D, System
+from scanner.repo.system_repository import SystemRepository
+from tests.facade import TestFacade
+
+
+@pytest.fixture
+def command(
+    system_repository: SystemRepository,
+):
+    return UpdateSystem(system_repository)
+
+
+def test_system_is_added(
+    command: UpdateSystem,
+    system_repository: SystemRepository,
+):
+    request = UpdateSystemRequest(
+        address=1,
+        name="New System",
+        position=Point3D(1, 1, 1),
+    )
+    command.execute(request)
+
+    system = system_repository.get_system_by_name("New System")
+
+    assert_that(
+        system,
+        equal_to(
+            System(
+                address=1,
+                name="New System",
+                position=Point3D(1, 1, 1),
+            )
+        ),
+    )
+
+
+def test_existing_system_is_ignored(
+    command: UpdateSystem,
+    system_repository: SystemRepository,
+    test_facade: TestFacade,
+):
+    test_facade.given_system(
+        address=1, name="Existing System", position=Point3D(0, 0, 0)
+    )
+    request = UpdateSystemRequest(
+        address=1,
+        name="Existing System",
+        position=Point3D(1, 1, 1),
+    )
+    command.execute(request)
+
+    system = system_repository.get_system_by_name("Existing System")
+
+    assert_that(
+        system,
+        equal_to(
+            System(
+                address=1,
+                name="Existing System",
+                position=Point3D(0, 0, 0),
+            )
+        ),
+    )
